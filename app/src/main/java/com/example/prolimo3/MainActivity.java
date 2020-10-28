fff068eb2abcc3f38b10f19e8135efeb97f5358b
JazminAndrade <72331184+jazminandrade@users.noreply.github.com>
2020-10-27 23:03:42 -0600

    Creación de progress bar y sus funciones mediante hilos

--- app/src/main/java/com/example/prolimo3/MainActivity.java
+++ app/src/main/java/com/example/prolimo3/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.prolimo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button siguiente;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        siguiente = (Button)findViewById(R.id.button);
+        siguiente.setOnClickListener(new View.OnClickListener (){
+            @Override
+            public void onClick(View v) {
+                Intent siguiente = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(siguiente);
+            }
+        });
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
--- app/src/main/java/com/example/prolimo3/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/prolimo3/ui/home/HomeFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,19 +21,85 @@ import com.example.prolimo3.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    View vista;
+    ToggleButton conectado;
+    ProgressBar señal;
+    ProgressBar bateria;
+    ProgressBar liquido;
+    int x = 100;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        vista = inflater.inflate(R.layout.fragment_home, container, false);
+        conectado = (ToggleButton)vista.findViewById(R.id.toggleButton);
+        señal = (ProgressBar)vista.findViewById(R.id.progressBar);
+        bateria = (ProgressBar)vista.findViewById(R.id.progressBar2);
+        liquido = (ProgressBar)vista.findViewById(R.id.progressBar3);
+
+        señal.setMax(100);
+        bateria.setMax(100);
+        liquido.setMax(100);
+
+        conectado.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    funcion(x);
+                    funcion2(x);
+                    funcion3(x);
+                }
             }
         });
-        return root;
+        return vista;
+    }
+    public void funcion(final int i){
+        señal.setProgress(i);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(10000);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                funcion(i-10);
+            }
+        });
+        thread.start();
+    }
+
+    public void funcion2(final int i){
+        bateria.setProgress(i);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(5000);
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+                funcion2(i-10);
+            }
+        });
+        thread.start();
+    }
+
+    public void funcion3(final int i){
+        liquido.setProgress(i);
+       // if (i==40){
+         //   Toast.makeText(getContext(), "Se está acabando el líquido",Toast.LENGTH_LONG).show();
+        //};
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    Thread.sleep(3000);
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                funcion3(i-10);
+            }
+        });
+        thread.start();
     }
 }
