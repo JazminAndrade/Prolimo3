fe00d5c6b13e60bba3e8cab7cd0f3f7bd56ed619
Vicente C <vicente2027@gmail.com>
2020-10-28 21:26:01 -0600

    Botón de Hello World con FireBase

--- app/src/main/java/com/example/prolimo3/MainActivity.java
+++ app/src/main/java/com/example/prolimo3/MainActivity.java
@@ -27,10 +27,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(siguiente);
             }
         });
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
     }
 }
